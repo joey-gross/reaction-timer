22f5b60609778ea4698c91e071b482188b16d4a6
joey-gross <hello@joeygross.com>
2022-08-06 13:14:14 -0500

    update component lifecycle hook

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="block">click me</div>
+  <div class="block" v-if="showBlock">click me</div>
 </template>
 
 <script>
 export default {
   props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log('components mounted');
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
 };
 </script>
 
